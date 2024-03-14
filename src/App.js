77ad7c20d521c55399867410bd167e99b4fdabee
Shubhodeep <banerjeeshubho98@gmail.com>
2024-03-14 20:36:06 +0530

    route work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
-import Test from './components/Test';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Test from "./components/Test";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
 
 function App() {
+
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route>
+      <Route>
+
+      </Route>
+    </Route>
+  ));
   return (
     <div className="App">
-     <Test/>
+      <Test />
     </div>
   );
 }
