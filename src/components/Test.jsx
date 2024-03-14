a0885f03e3a5436119d34b4294e0581e366c48a6
Shubhodeep <banerjeeshubho98@gmail.com>
2024-03-14 18:15:15 +0530

    project cleanup

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,43 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-const ExampleComponent = () => {
-  const [count, setCount] = useState(0);
-  const [message, setMessage] = useState("");
+function Test() {
+  return <div>This is REDUX.</div>;
+}
 
-  useEffect(() => {
-    // This effect will run after every render
-    console.log("Effect ran");
-  });
-
-  useEffect(() => {
-    // This effect will only run once, after the initial render
-    console.log("Initial effect ran");
-  }, []);
-
-  useEffect(() => {
-    // This effect will run whenever the 'count' state changes
-    console.log("Count changed:", count);
-  }, [count]);
-
-  useEffect(() => {
-    // This effect will run whenever the 'message' state changes
-    console.log("Message changed:", message);
-  }, [message]);
-
-  return (
-    <div>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <br />
-      <input
-        type="text"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-        placeholder="Type something..."
-      />
-      <p>Message: {message}</p>
-    </div>
-  );
-};
-
-export default ExampleComponent;
+export default Test;
