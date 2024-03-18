5c5779e9458cb7818ec3483b813fcdfca079db8a
Shubhodeep <banerjeeshubho98@gmail.com>
2024-03-18 23:58:45 +0530

    NavBar work

--- src/components/RootLayout.js
+++ src/components/RootLayout.js
@@ -1,11 +1,20 @@
 import { Outlet } from "react-router-dom";
+import NavBar from "./NavBarPannel";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+
 
 function RootLayout() {
   return (
     <>
-      <h1>Navigation</h1>
+      <NavBar />
       <main>
-        <Outlet/>
+        <Outlet />
       </main>
     </>
   );
