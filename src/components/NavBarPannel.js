b51de994883587b7abf8465830a985bd71be7e5b
Shubhodeep <banerjeeshubho98@gmail.com>
2024-03-19 22:39:41 +0530

    NavBar work

--- src/components/NavBarPannel.js
+++ src/components/NavBarPannel.js
@@ -10,7 +10,9 @@ function NavBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
-        <Navbar.Brand href="#">REDUX Ecommerce</Navbar.Brand>
+        <Navbar.Brand href="#">
+          REDUX Ecommerce
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
@@ -20,19 +22,7 @@ function NavBar() {
           >
             <Nav.Link href="#action1">Products</Nav.Link>
 
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
           </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
