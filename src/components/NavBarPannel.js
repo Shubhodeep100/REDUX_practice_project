967e7ccd62ce4baef8e0a1dba1bbecaa284d53ad
Shubhodeep <banerjeeshubho98@gmail.com>
2024-03-20 11:34:28 +0530

    Outlet component addition

--- src/components/NavBarPannel.js
+++ src/components/NavBarPannel.js
@@ -1,18 +1,13 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-
+import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
-        <Navbar.Brand href="#">
-          REDUX Ecommerce
-        </Navbar.Brand>
+        <Navbar.Brand href="#">REDUX Ecommerce</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
@@ -20,8 +15,9 @@ function NavBar() {
             style={{ maxHeight: "100px" }}
             navbarScroll
           >
-            <Nav.Link href="#action1">Products</Nav.Link>
-
+            <Nav.Link href="/" as={Link}>
+              Products
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
