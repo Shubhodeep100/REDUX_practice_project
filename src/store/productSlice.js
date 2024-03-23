174a5e393b9f98c114d8055c6563e10d9d136271
Shubhodeep <banerjeeshubho98@gmail.com>
2024-03-24 01:30:10 +0530

    creating Thunk Middleware

--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -13,21 +13,20 @@ const productSlice = createSlice({
     // },
   },
   extraReducers: (builder) => {
-builder
-.addCase(getProducts.fulfilled, (state, action) => {
-  state.data = action.payload;
-})
-  }
+    builder.addCase(getProducts.fulfilled, (state, action) => {
+      state.data = action.payload;
+    });
+  },
 });
 
 export const { fetchProducts } = productSlice.actions;
 export default productSlice.reducer;
 
-export const getProducts = createAsyncThunk('products/get', async () => {
+export const getProducts = createAsyncThunk("products/get", async () => {
   const data = await fetch("https://fakestoreapi.com/products");
   const result = await data.json();
   return result;
-})
+});
 
 // export function getProducts() {
 //   return async function getProductsThunk(dispatch, getState) {
